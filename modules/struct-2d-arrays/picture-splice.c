15cfd010ff837cda4c9efb07afd09d72ffb5aa5e
liuanqin <liuanqin@grinnell.edu>
2014-11-08 13:35:19 -0600

    Oct 15 and Oct 27 modified

--- modules/struct-2d-arrays/picture-splice.c
+++ modules/struct-2d-arrays/picture-splice.c
@@ -10,26 +10,26 @@
  *******/
 
 #include <stdio.h>
-#include "MyroC.h"
+#include "MyroCDev.h"
 
 
 int
 main()
 {
   int i, j, width, height, midwidth, midheight;
-  Picture * pic1, * pic2, * spliced;
+  Picture  pic1,  pic2,  spliced;
   rConnect ("/dev/rfcomm0");
 
   pic1 = rTakePicture();
-  width = rGetPictureWidth (pic1);
-  height = rGetPictureHeight (pic1);
+  width = pic1.width;
+  height = pic1.height;
   midwidth = width / 2;
   midheight = height / 2;
   rTurnLeft (1, 1);
   pic2 = rTakePicture();
   spliced = rTakePicture();
-  rDisplayPicture (pic1, "pic1");
-  rDisplayPicture (pic2, "pic2");
+  rDisplayPicture (pic1, 5, "pic1");
+  rDisplayPicture (pic2, 5, "pic2");
 
   for (i = 0; i < width ; i++)
     {
@@ -37,14 +37,14 @@ main()
         {
           if ( ( (i < midwidth) && (j < midheight))
                || ( (i > midwidth) && (j > midheight)) )
-            rSetPicturePixel (spliced, i, j, (rGetPicturePixel (pic1, i, j)));
-          else
-            rSetPicturePixel (spliced, i, j, (rGetPicturePixel (pic2, i, j)));
+          spliced.pix_array[i][j]=pic1.pix_array[i][j];           
+else
+            spliced.pix_array[i][j]=pic2.pix_array[i][j]; 
 
         }
     }
 
-  rShowPicture (spliced);
+  rDisplayPicture (spliced,-3,"spliced");
   rDisconnect();
   return 0;
 }
